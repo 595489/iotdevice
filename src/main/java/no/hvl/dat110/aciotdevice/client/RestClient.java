2fc5750f71723310d4118b6a2ce9ea0de72fb305
Isak <595489@stud.hvl.no>
2022-05-06 13:09:36 +0200

    OkHttp added to doPost in RestClient

--- src/main/java/no/hvl/dat110/aciotdevice/client/RestClient.java
+++ src/main/java/no/hvl/dat110/aciotdevice/client/RestClient.java
@@ -1,8 +1,6 @@
 package no.hvl.dat110.aciotdevice.client;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import okhttp3.*;
 import com.google.gson.Gson;
 
 import java.io.IOException;
@@ -18,6 +16,21 @@ public class RestClient {
 	public void doPostAccessEntry(String message) {
 
 		// TODO: implement a HTTP POST on the service to post the message
+		OkHttpClient client = new OkHttpClient().newBuilder()
+				.build();
+		MediaType mediaType = MediaType.parse("text/plain");
+		RequestBody body = RequestBody.create(mediaType, "locked");
+		Request request = new Request.Builder()
+				.url("http://localhost:8080/accessdevice/log/")
+				.method("POST", body)
+				.addHeader("Content-Type", "text/plain")
+				.build();
+		try (Response response = client.newCall(request).execute()){
+			
+		}
+		catch (IOException e){
+			e.printStackTrace();
+		}
 		
 	}
 	
@@ -37,7 +50,7 @@ public class RestClient {
 				.method("GET", null)
 				.build();
 		try (Response response = client.newCall(request).execute()){
-			
+
 		}
 		catch (IOException e){
 			e.printStackTrace();
